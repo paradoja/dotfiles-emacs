53c48a6462f0228c02fa4c07b16d39571c8e993e
Abby Henríquez Tejera <paradoja@gmail.com>
2016-07-30 13:05:39 +0200

    Fixed JS loading

--- emacs.d/prog/javascript.el
+++ emacs.d/prog/javascript.el
@@ -1,6 +1,4 @@
 (require 'use-package)
 
-(use-package js-mode-hook
-  :init
-  (add-hook 'js-mode-hook (lambda () (tern-mode t))))
 (use-package tern)
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
