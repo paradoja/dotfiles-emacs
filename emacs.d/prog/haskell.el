83d6347067112d4c505b6b5f9f04c1f60ccbdb76
Abby Henríquez Tejera <paradoja@gmail.com>
2016-08-11 16:40:36 +0200

    Testing new haskell mode

--- emacs.d/prog/haskell.el
+++ emacs.d/prog/haskell.el
@@ -1,28 +1,32 @@
 (require 'use-package)
 (use-package haskell-mode
   :init
-  (add-hook 'haskell-mode-hook
-            (lambda ()
-              (require 'haskell-indent)
-              (interactive-haskell-mode)
-              (turn-on-haskell-doc-mode)
-              (turn-on-haskell-indent)
-              (capitalized-words-mode t)
-              (setf haskell-program-name "ghci")
-              (define-key haskell-mode-map (kbd "C-<right>")
-                (lambda ()
-                  (interactive)
-                  (haskell-move-nested 1)))
-              ;; Same as above but backwards.
-              (define-key haskell-mode-map (kbd "C-<left>")
-                (lambda ()
-                  (interactive)
-                  (haskell-move-nested -1)))
-              (structured-haskell-mode)))
+  ;; (add-hook 'haskell-mode-hook
+  ;;           (lambda ()
+  ;;             (require 'haskell-indent)
+  ;;             (interactive-haskell-mode)
+  ;;             (turn-on-haskell-doc-mode)
+  ;;             (turn-on-haskell-indent)
+  ;;             (capitalized-words-mode t)
+  ;;             (setf haskell-program-name "ghci")
+  ;;             (define-key haskell-mode-map (kbd "C-<right>")
+  ;;               (lambda ()
+  ;;                 (interactive)
+  ;;                 (haskell-move-nested 1)))
+  ;;             ;; Same as above but backwards.
+  ;;             (define-key haskell-mode-map (kbd "C-<left>")
+  ;;               (lambda ()
+  ;;                 (interactive)
+  ;;                 (haskell-move-nested -1)))
+  ;;             (structured-haskell-mode)))
 
   (add-hook 'inferior-haskell-mode-hook
             (lambda ()
               (capitalized-words-mode t))))
 
+(use-package intero
+  :init
+  (add-hook 'haskell-mode-hook 'intero-mode))
+
 (use-package ghci-completion)
-(use-package shm)
+;(use-package shm)
