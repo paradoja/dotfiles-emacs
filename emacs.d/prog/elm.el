ae657e13817970d8d176558d0ea661027cf325c4
Abby Henríquez Tejera <paradoja@gmail.com>
2016-09-06 23:06:32 +0200

    Updated elm mode

--- emacs.d/prog/elm.el
+++ emacs.d/prog/elm.el
@@ -1,3 +1,10 @@
 (require 'use-package)
 
-(use-package elm-mode)
+(use-package elm-mode
+  :config
+  (add-hook 'elm-mode-hook #'elm-oracle-setup-ac))
+
+(use-package flycheck-elm
+  :config
+  (eval-after-load 'flycheck
+    '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup)))
