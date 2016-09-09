e46472950759ba1dfd34bae7cd4169d642f92bb1
Abby Henríquez Tejera <paradoja@gmail.com>
2016-09-09 18:25:17 +0200

    Rust with company mode

--- emacs.d/prog/rust.el
+++ emacs.d/prog/rust.el
@@ -3,3 +3,11 @@
 (use-package rust-mode
   :mode "\\.rs\\'")
 (use-package flycheck-rust)
+(use-package racer
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode))
+
+; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+; (setq company-tooltip-align-annotations t)
