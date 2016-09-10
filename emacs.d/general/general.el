529c45dd79bb7a41e6d49e079662f645f727991b
Abby Henríquez Tejera <paradoja@gmail.com>
2016-09-10 02:13:41 +0200

    Adding irony for c(++)

--- emacs.d/general/general.el
+++ emacs.d/general/general.el
@@ -124,18 +124,26 @@
  'smart-mode-line-dark
  '(mode-line ((t :foreground "gray60" :background "#404045")))
  '(mode-line-inactive     ((t :foreground "gray60" :background "black"))))
-(add-to-list 'sml/replacer-regexp-list '("^~/.homesick/repos/emacs-files.git/home/.emacs.d/" ":ED:"))
+(add-to-list 'sml/replacer-regexp-list `(,(concat "^" (abbreviate-file-name (file-truename "~/.emacs.d"))) ":ED:"))
 (mapc (lambda (mode)
         (add-to-list 'sml/hidden-modes (format " %s" mode)))
-      (list "Paredit"    ; paredit-mode
-            "FlyC"       ; flycheck-mode
-            "Undo-Tree"  ; undo-tree-mode
-            "yas"        ; yas/minor-mode
-            "rt"         ; ruby-tools-mode
-            "REl"        ; ruby-electric-mode
-            "Guide"      ; guide-key-mode
-            "Helm"       ; helm
-            ))
+      (list
+       "Abbrev"     ; Abbrev
+       "Arev"       ; auto-revert
+       "ElDoc"      ; eldoc-mode
+       "FlyC"       ; flycheck-mode
+       "Guide"      ; guide-key-mode
+       "Helm"       ; helm
+       "Paredit"    ; paredit-mode
+       "Undo-Tree"  ; undo-tree-mode
+       "company"    ; company-mode
+       "rt"         ; ruby-tools-mode
+       "yas"        ; yas/minor-mode
+
+       ; specific languages
+       "Irony"      ; c-related irony-mode
+       "REl"        ; ruby-electric-mode
+       ))
 (setq-default sml/col-number-format  "%3l")
 (setq-default sml/line-number-format "%3l")
 (setq-default sml/numbers-separator  "")
--- emacs.d/prog/c.el
+++ emacs.d/prog/c.el
@@ -1,8 +1,43 @@
-(add-hook 'c-mode-hook
-          '(lambda ()
-             (c-set-style "k&r")
-             (c-toggle-hungry-state 1)
-             (setq c-auto-newline 1)
-             (auto-fill-mode 1)
-             (define-key c-mode-map (kbd "<return>")
-               'pdox-new-line-and-indent)))
+(require 'use-package)
+
+(mapc (lambda (language)
+        (let (language-mode (intern (concat (symbol-name language) "-mode-hook")))
+         (add-hook 'c-mode-hook
+                   '(lambda ()
+                      (c-set-style "stroustrup")
+                      (c-toggle-hungry-state 1)
+                      (set-default c-auto-newline 1)
+                      (auto-fill-mode 1)
+                      (define-key c-mode-map (kbd "<return>")
+                        'pdox-new-line-and-indent)))))
+      (list 'c 'c++))
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+
+;; Requires clang & cmake
+(use-package irony
+  :config
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+
+(use-package company-irony
+  :config
+  (eval-after-load 'company
+    '(add-to-list 'company-backends 'company-irony)))
+
+(use-package flycheck-irony
+  :config
+  (eval-after-load 'flycheck
+    '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
+
+(use-package irony-eldoc
+  :config
+  (add-hook 'irony-mode-hook 'irony-eldoc))
