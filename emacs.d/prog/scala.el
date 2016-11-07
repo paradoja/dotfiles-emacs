73731d09ed839bebb94dda7a23b53cd2db085042
Abby Henríquez Tejera <paradoja@gmail.com>
2016-11-07 18:36:47 +0100

    Adapted scala.el

--- emacs.d/prog/scala.el
+++ emacs.d/prog/scala.el
@@ -1,12 +1,4 @@
 (require 'use-package)
-(use-package scala-mode)
-(use-package ensime)
-(use-package sbt-mode)
 
-
-(eval-after-load "scala-mode"
-  '(progn
-     (define-key scala-mode-map (kbd "RET") 'newline-and-indent)))
-
-(require 'ensime)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(use-package ensime
+  :pin melpa)
