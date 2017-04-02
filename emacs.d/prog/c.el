287e84b1ffb0dddadf95a5ad03acdebc0d5206ff
Abby Henríquez Tejera <paradoja@gmail.com>
2017-04-02 22:57:00 +0200

    Added rtags

--- emacs.d/prog/c.el
+++ emacs.d/prog/c.el
@@ -41,3 +41,17 @@
 (use-package irony-eldoc
   :config
   (add-hook 'irony-mode-hook 'irony-eldoc))
+
+(use-package rtags
+  :config
+  (setq rtags-completions-enabled t)
+  (setq rtags-autostart-diagnostics t)
+  (rtags-enable-standard-keybindings))
+(require 'company-rtags)
+
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends 'company-rtags))
+
+(require 'rtags-helm)
+(setq rtags-use-helm t)
