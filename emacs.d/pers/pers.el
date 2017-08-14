80143e97d0185ffaf50aa2fc5870f02c3c4e4abb
Abby Henríquez Tejera <abby@coya.insure>
2017-08-14 12:12:47 +0200

    Added work org to config

--- emacs.d/pers/pers.el
+++ emacs.d/pers/pers.el
@@ -1,6 +1,10 @@
+(require 'config-utils)
+
 ;;; Computer specific
 (if-exists-load-file "~/.emacs-local")
 
+;;; Work related org
+(if-exists-load-file "~/org/.emacs-work")
 
 ;;; MacOSX
 (if (or (string-match "i386-apple-darwin.*" system-configuration)
