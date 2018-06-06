9f390a14797aebc838d629dedba59cef772af7ca
Abby Henríquez Tejera <abby@coya.insure>
2018-06-06 15:47:13 +0200

    Updated js configuration

--- emacs.d/prog/javascript.el
+++ emacs.d/prog/javascript.el
@@ -1,4 +1,21 @@
 (require 'use-package)
 
+(use-package js2-mode
+  :config
+  (progn (use-package js2-refactor)
+         (use-package xref-js2)))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode) ; in case we load the menu
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+
+;; (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
 (use-package tern)
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+(add-hook 'js2-mode-hook #'aggressive-indent-mode)
