7adfa2345b2bf1d7637f1a895745de2173c21887
Abby Henríquez Tejera <paradoja@gmail.com>
2018-12-19 14:43:39 +0100

    Adding lisp-demos

--- emacs.d/prog/elisp.el
+++ emacs.d/prog/elisp.el
@@ -3,3 +3,7 @@
 (add-to-list 'rtog/mode-repl-alist '(purescript-mode . psci))
 (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+
+(use-package elisp-demos)
+(advice-add 'helpful-update
+            :after #'elisp-demos-advice-helpful-update)
