60d7f63c13fed1ab5ca82b4a7cf7808e9fd20f63
Abby Henríquez Tejera <paradoja@gmail.com>
2018-12-21 13:59:09 +0100

    Added new git modes

--- emacs.d/tools/magit.el
+++ emacs.d/tools/magit.el
@@ -1,6 +1,7 @@
 (require 'use-package)
 (use-package magit)
 (require 'magit)
+(require 'config-utils)
 
 (global-set-key (kbd "C-,") 'magit-status)
 (defvar magit-last-seen-setup-instructions "1.4.0")
@@ -8,6 +9,11 @@
 (add-hook 'git-gutter:update-hooks 'magit-revert-buffer-hook)
 (add-hook 'git-commit-mode-hook 'git-commit-turn-on-flyspell)
 
-(use-package git-link)
-(use-package git-gutter)
-(use-package forge)
+(use-packages
+  git-link
+  git-gutter
+  forge
+  git
+  gitattributes-mode
+  gitconfig-mode
+  gitignore-mode)
