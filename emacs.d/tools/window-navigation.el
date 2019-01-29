1c9464cc0c4bd7f6d95a8cca727b26a95a253f2a
Abby Henríquez Tejera <paradoja@gmail.com>
2019-01-29 17:14:05 +0100

    Window navigation now allows easy swapping and deletion of windows

--- emacs.d/tools/window-navigation.el
+++ emacs.d/tools/window-navigation.el
@@ -18,8 +18,9 @@
                             ((char-equal ?t option) #'rotate:tiled))))
 
 (defun other-window-directed (option arg)
-  "Changes the point to the selected window."
-  (interactive "c[o] other window [0-9] go to window 0-9 [hjkl]/[bnpf] move point ←↑↓→ [R] change window size [r] Rotate\nP")
+  "Changes the point to the selected window. With C-u swap
+windows, and with C-u C-u delete the window."
+  (interactive "c[o] other window [0-9] to window 0-9 [hjkl]/[bnpf] point ←↑↓→ [R] change window size [r] Rotate\np")
   (let ((case-fold-search nil))
     (cl-flet* ((char-in (char list)
                         (-any? (lambda (current-char) (char-equal char current-char))
@@ -33,14 +34,42 @@
              (call-interactively #'other-window))
             ((and (<= ?0 option)
                   (>= ?9 option))
-             (select-window-by-number (string-to-number (char-to-string option))
-                                      arg))
+             (let ((number (string-to-number (char-to-string option))))
+               (message "%s" option)
+               (if (= arg 4)
+                   (window-navigation-switch-current-window-with-numbered-window number)
+                 (select-window-by-number number
+                                          (if (= arg 16) 4)))))
             ((move-point-towards option)
              (window-jump (move-point-towards option)))
             ((char-equal ?R option)
              (hydra-increase-window-size/body))
             ((char-equal ?r option)
              (call-interactively #'do-window-rotation))))))
+
+(defun window-navigation-switch-current-window-with-numbered-window (i)
+  "Switches the current window in the frame with another window
+given with the window-navigation numbering."
+  (let* ((frame (selected-frame))
+         (windows (car (gethash frame window-numbering-table)))
+         (other-window (aref windows i))
+         (current-window (frame-selected-window frame))
+         (original-buffer (current-buffer))
+         other-buffer)
+    (select-window other-window)
+    (setq other-buffer (current-buffer))
+    (switch-to-buffer original-buffer)
+    (select-window current-window)
+    (switch-to-buffer other-buffer)))
+
+(let ((w (frame-selected-window))
+      (b (current-buffer))
+      b2)
+  (select-window (aref (car (gethash (selected-frame) window-numbering-table)) 1))
+  (setq b2 (current-buffer))
+  (switch-to-buffer b)
+  (select-window w)
+  (switch-to-buffer b2))
 
 
 (defhydra hydra-increase-window-size ()
