f770ef04931d87e3ea2ffabe565998a16e0f6a59
Abby Henríquez Tejera <paradoja@gmail.com>
2019-02-05 14:17:31 +0100

    Changed fly/Ispell default language

--- emacs.d/tools/spelling-check.el
+++ emacs.d/tools/spelling-check.el
@@ -2,6 +2,9 @@
 
 (use-package helm-flyspell)
 
+(ispell-change-dictionary "en_GB" t) ; default dict
+
+(define-key global-map (kbd "C-.") 'helm-flyspell-correct)
 (define-key flyspell-mode-map (kbd "C-.") 'helm-flyspell-correct)
 (define-key flyspell-mode-map (kbd "C-M-i") 'helm-flyspell-correct)
 (define-key flyspell-mode-map (kbd "C-,") nil)
