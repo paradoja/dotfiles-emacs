03e3025e88ab29560de07ed1d5bae70438eb202f
Abby Henríquez Tejera <paradoja@gmail.com>
2019-02-15 18:26:15 +0100

    Racket mode added

--- emacs.d/prog/racket.el
+++ emacs.d/prog/racket.el
@@ -1,5 +1,22 @@
 (require 'use-package)
+(require 'repl-toggle)
 
-(use-package geiser)
-(use-package quack)
-(add-hook 'racket-mode-hook #'aggressive-indent-mode)
+(use-package racket-mode
+  :general
+  (racket-mode-map
+   "C-c z" 'racket-run
+   "C-c C-z" 'rtog/toggle-repl
+   "{"  'paredit-open-curly
+   "}" 'paredit-close-curly
+   "M-[" 'paredit-wrap-square
+   "M-{" 'paredit-wrap-curly)
+  :hook
+  ((racket-unicode-input-method-enable . racket-mode)
+   (racket-unicode-input-method-enable . racket-repl-mode))
+  :config
+  (progn
+    (add-to-list 'rtog/mode-repl-alist '(racket-mode . racket-run-and-switch-to-repl))
+    (add-hook 'racket-mode-hook #'aggressive-indent-mode)))
+
+;; future custom build step:
+;; (racket-mode-start-faster)
