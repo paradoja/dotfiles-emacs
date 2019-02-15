a13d451abbaa89ef29aa7ed7d9bba990d8b39956
Abby Henríquez Tejera <paradoja@gmail.com>
2019-02-15 10:20:32 +0100

    Fix ielm loading for elisp

--- emacs.d/prog/elisp.el
+++ emacs.d/prog/elisp.el
@@ -1,9 +1,14 @@
+(require 'use-package)
 (require 'repl-toggle)
 
-(add-to-list 'rtog/mode-repl-alist '(purescript-mode . psci))
+(use-package ielm)
+(add-to-list 'rtog/mode-repl-alist '(emacs-lisp-mode . ielm))
+
 (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
 
 (use-package elisp-demos)
 (advice-add 'helpful-update
             :after #'elisp-demos-advice-helpful-update)
+
+(provide 'elisp)
