95216dc017a62165bcc9bbcf4689b27ef62a772b
Abby Henríquez Tejera <paradoja@gmail.com>
2019-05-21 10:33:42 +0200

    Added restclient-ob & -helm

--- emacs.d/tools/restclient.el
+++ emacs.d/tools/restclient.el
@@ -1,3 +1,10 @@
 (require 'use-package)
 
 (use-package restclient)
+(use-package restclient-helm)
+
+(use-package ob-restclient)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ (append org-babel-load-languages
+         '((restclient . t))))
