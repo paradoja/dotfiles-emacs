977dd89fe6863eb380b07581ada3b18d31fc538f
Abby Henríquez Tejera <paradoja@gmail.com>
2019-09-08 22:07:11 +0200

    Erlang mode only available if erl is available

--- emacs.d/prog/erlang.el
+++ emacs.d/prog/erlang.el
@@ -1,11 +1,12 @@
 (require 'use-package)
 
-(use-package erlang
-  :init
-  (let* ((exec (f-canonical (executable-find "erl")))
-         (bin-dir (f-dirname exec))
-         (root-dir (f-dirname bin-dir))
-         (man-dir (f-join root-dir "man")))
-    (setq erlang-root-dir root-dir)
-    (add-to-list 'exec-path bin-dir)
-    (setq erlang-man-root-dir man-dir)))
+(when (executable-find "erl")
+  (use-package erlang
+    :init
+    (let* ((exec (f-canonical (executable-find "erl")))
+           (bin-dir (f-dirname exec))
+           (root-dir (f-dirname bin-dir))
+           (man-dir (f-join root-dir "man")))
+      (setq erlang-root-dir root-dir)
+      (add-to-list 'exec-path bin-dir)
+      (setq erlang-man-root-dir man-dir))))
