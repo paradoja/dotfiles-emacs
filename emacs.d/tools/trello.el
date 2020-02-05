df006ef8017800061350d54e6100a3c4a7aca588
Abby Henríquez Tejera <paradoja@gmail.com>
2020-02-05 16:52:28 +0100

    trello: Configuration set

--- emacs.d/tools/trello.el
+++ emacs.d/tools/trello.el
@@ -5,8 +5,11 @@
   :config
   (setq org-trello--config-dir
         (f-join user-emacs-directory "tools" "trello")
+        ;; normally `org-trello-config-file` will depend on the
+        ;; username (with a placeholder `%s`), but that's not the case
+        ;; here
         org-trello--config-file
-        (f-join user-emacs-directory "tools" "trello" "%s.el"))
+        (f-join user-emacs-directory "tools" "trello" "conf.el"))
   :custom
   (org-trello-files
    (list (f-join org-directory "main" "trello.org"))
