770dd4e97c7ffba6777b6bb8c7495a7a5d2a8cfd
Abby Henríquez Tejera <paradoja@gmail.com>
2020-04-14 23:50:46 +0200

    Updated haskell packages

--- emacs.d/prog/haskell.el
+++ emacs.d/prog/haskell.el
@@ -1,42 +1,9 @@
 (require 'use-package)
-(use-package haskell-mode
-  :init
-  ;; (add-hook 'haskell-mode-hook
-  ;;           (lambda ()
-  ;;             (require 'haskell-indent)
-  ;;             (interactive-haskell-mode)
-  ;;             (turn-on-haskell-doc-mode)
-  ;;             (turn-on-haskell-indent)
-  ;;             (capitalized-words-mode t)
-  ;;             (setf haskell-program-name "ghci")
-  ;;             (define-key haskell-mode-map (kbd "C-<right>")
-  ;;               (lambda ()
-  ;;                 (interactive)
-  ;;                 (haskell-move-nested 1)))
-  ;;             ;; Same as above but backwards.
-  ;;             (define-key haskell-mode-map (kbd "C-<left>")
-  ;;               (lambda ()
-  ;;                 (interactive)
-  ;;                 (haskell-move-nested -1)))
-  ;;             (structured-haskell-mode)))
 
-  (add-hook 'inferior-haskell-mode-hook
-            (lambda ()
-              (capitalized-words-mode t))))
+;; requires haskell ide engine (hie)
+;; https://github.com/haskell/haskell-ide-engine#installation
 
-(use-package intero
-  :init
-  (progn
-    (add-hook 'haskell-mode-hook 'intero-mode)
-    (add-to-list 'rtog/mode-repl-alist '(haskell-mode . intero-repl))))
-
-(require 'flycheck)
-(flycheck-add-next-checker 'intero '(warning . haskell-hlint))
-
-(use-package ghci-completion)
-(use-package hindent)
-(require 'hindent)
-(add-hook 'haskell-mode-hook #'hindent-mode)
-;;; (use-package shm)
-
-(use-package shakespeare-mode) ; for Yesod's templates
+(use-package haskell-mode)
+(use-package lsp-haskell
+  :hook (haskell-mode))
+;; (use-package shakespeare-mode) ; for Yesod's templates
