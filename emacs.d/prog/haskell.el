bf74ca1085606d5629ca1594b1477bae02575a44
Abby Henríquez Tejera <paradoja@gmail.com>
2020-05-10 18:07:46 +0200

    Adapted haskell mode

--- emacs.d/prog/haskell.el
+++ emacs.d/prog/haskell.el
@@ -2,12 +2,34 @@
 
 ;; requires haskell ide engine (hie)
 ;; https://github.com/haskell/haskell-ide-engine#installation
+;; requires hastags for tags
 
 (use-package haskell-mode
-  :hook (interactive-haskell-mode)
+  :hook
+  ((interactive-haskell-mode)
+   (haskell-mode .
+                 (lambda ()
+                   (set (make-local-variable 'company-backends)
+                        (append '((company-capf company-dabbrev-code))
+                                company-backends)))))
   :config
   (when (executable-find "brittany")
-    (add-hook 'haskell-mode-hook #'format-all-mode)))
+    (add-hook 'haskell-mode-hook #'format-all-mode))
+  :custom
+  ((haskell-tags-on-save  t)
+   (haskell-compile-cabal-build-command "stack build")
+   (haskell-process-suggest-remove-import-lines t)
+   (haskell-process-auto-import-loaded-modules t)
+   (haskell-process-log t))
+  :general
+  (:prefix "C-c C-p"
+           "C-l" 'haskell-process-load-or-reload
+           "i" 'haskell-interactive-bring
+           "C-t" 'haskell-process-do-type
+           "C-i" 'haskell-process-do-info
+           "C-c" 'haskell-process-cabal-build
+           "C-k" 'haskell-interactive-mode-clear
+           "c" 'haskell-process-cabal))
 (use-package lsp-haskell
   :hook (haskell-mode))
 ;; (use-package shakespeare-mode) ; for Yesod's templates
