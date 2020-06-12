1dc801c15594e14eb8ada6a35e2243888015a59e
Abby Henríquez Tejera <paradoja@gmail.com>
2020-06-12 17:07:09 +0200

    Added LSP for scala and lua

--- emacs.d/prog/lua.el
+++ emacs.d/prog/lua.el
@@ -1,5 +1,23 @@
 (require 'use-package)
 
+(requirements-add
+ lua.el
+ (lua-lsp
+  (executable-find "lua-lsp")
+  "Lua lsp
+   luarocks install --local --server=http://luarocks.org/dev lua-lsp"
+  "https://github.com/Alloyed/lua-lsp")
+ (luacheck
+  (executable-find "luacheck")
+  "Lua linter
+   luarocks install --local luacheck"
+  "https://github.com/mpeterv/luacheck")
+ (lcf
+  (executable-find "lua.reformat")
+  "Lua formatter
+   luarocks install --local lcf"
+  "https://luarocks.org/modules/martin-eden/lcf"))
+
 (use-package lua-mode)
 (use-package company-lua
   :config
