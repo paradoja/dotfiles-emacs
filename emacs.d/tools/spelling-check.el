8a601fd08681b0c86dc661832fe6747cd010c63a
Abby Henríquez Tejera <paradoja@gmail.com>
2020-06-21 11:51:14 +0200

    spelling-check.el: Updated spelling-check dictionary info

--- emacs.d/tools/spelling-check.el
+++ emacs.d/tools/spelling-check.el
@@ -8,7 +8,7 @@
 
 (use-package helm-flyspell)
 
-(ispell-change-dictionary "en_GB" t) ; default dict
+(ispell-change-dictionary "british" t) ; default dict
 
 (define-key global-map (kbd "C-.") 'helm-flyspell-correct)
 (define-key flyspell-mode-map (kbd "C-.") 'helm-flyspell-correct)
