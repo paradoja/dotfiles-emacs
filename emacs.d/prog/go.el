8bce9e21f835f1f8e040c0a25d540ceb1d7d225d
Abby Henríquez Tejera <paradoja@gmail.com>
2020-10-27 16:31:11 +0100

    go.el: Added gopls

--- emacs.d/prog/go.el
+++ emacs.d/prog/go.el
@@ -1,20 +1,34 @@
 (require 'use-package)
 
+(requirements-add
+ go.el
+ (gopls
+  (executable-find "gopls")
+  "gopls"
+  "https://github.com/golang/tools/blob/master/gopls/doc/user.md"
+  "go get golang.org/x/tools/gopls"))
+
 (use-package go-mode
-  :config
-  (add-hook 'go-mode-hook
-            #'(lambda ()
-                ;; Use goimports instead of go-fmt
-                (setq gofmt-command "goimports")
-                ;; Customize compile command to run go build
-                (if (not (string-match "go" compile-command))
-                    (set (make-local-variable 'compile-command)
-                         "go build -v && go test -v && go vet"))
-                ;; Godef jump key binding
-                (add-hook 'before-save-hook 'gofmt-before-save)
-                (local-set-key (kbd "M-.") 'godef-jump)
-                (local-set-key (kbd "M-*") 'pop-tag-mark))))
+  :hook
+  (go-mode .
+           (lambda ()
+             ;; Use goimports instead of go-fmt
+             (setq gofmt-command "goimports")
+             ;; Customize compile command to run go build
+             (if (not (string-match "go" compile-command))
+                 (set (make-local-variable 'compile-command)
+                      "go build -v && go test -v && go vet"))
+             ;; Godef jump key binding
+             (add-hook 'before-save-hook 'gofmt-before-save)
+             (local-set-key (kbd "M-.") 'godef-jump)
+             (local-set-key (kbd "M-*") 'pop-tag-mark)
+             (lsp-deferred))))
 
 (use-package go-guru)
 (use-package company-go)
 (use-package flycheck-golangci-lint)
+
+(add-hook 'go-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'lsp-format-buffer t t)
+            (add-hook 'before-save-hook #'lsp-organize-imports t t)))
